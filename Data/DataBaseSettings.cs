ceb21baa4d0518f98de1f96a1c4f134fb81bc625
saitejachandala <104210102+saitejachandala@users.noreply.github.com>
2023-11-18 13:33:54 +0530

    Add files via upload

---
+++ Data/DataBaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiMongoDB.Data
+{
+    public class DataBaseSettings
+    {
+        public string CollectionName { get; set; }
+        public string DatabaseName { get; set; }
+        public string Connection { get; set; }
+    }
+}
